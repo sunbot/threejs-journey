ee4e66edcc132495562577a6270f9372332af958
Angela <angsunrey@gmail.com>
2021-03-02 10:39:35 -0600

    Exercise

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -10,10 +10,38 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+//space, shape, house
+const group = new THREE.Group();
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0xff0000})
+)
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0x00ff00})
+)
+cube2.position.x = -2;
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:0x0000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
+group.rotation.y = 1
+group.position.y = 1;
+group.scale.y = 2;
+
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -28,7 +56,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+
+
 scene.add(camera)
+// mesh.position.normalize()
+// camera.position.normalize()
 
 /**
  * Renderer
